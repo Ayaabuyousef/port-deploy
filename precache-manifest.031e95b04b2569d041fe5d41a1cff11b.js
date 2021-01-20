f7ca7e183f878c32d360776a4daac09fc7bffbe4
aya abuyousef <aya.abuyousef@gmail.com>
2021-01-20 17:34:50 +0200

    Updates

---
+++ precache-manifest.031e95b04b2569d041fe5d41a1cff11b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6ce97a6bddef3eadab0e947314f477",
+    "url": "/port-deploy/index.html"
+  },
+  {
+    "revision": "7ac19cedea778e23f620",
+    "url": "/port-deploy/static/css/2.efd9d441.chunk.css"
+  },
+  {
+    "revision": "e0f6ca8e68dec46e196c",
+    "url": "/port-deploy/static/css/main.21721e4e.chunk.css"
+  },
+  {
+    "revision": "7ac19cedea778e23f620",
+    "url": "/port-deploy/static/js/2.a4abcc2a.chunk.js"
+  },
+  {
+    "revision": "e0f6ca8e68dec46e196c",
+    "url": "/port-deploy/static/js/main.c55ba461.chunk.js"
+  },
+  {
+    "revision": "4062f69f9e0ee2ea8816",
+    "url": "/port-deploy/static/js/runtime-main.8de50a40.js"
+  },
+  {
+    "revision": "aa7581c943bf241bdb7ee95822da621f",
+    "url": "/port-deploy/static/media/cv.aa7581c9.png"
+  },
+  {
+    "revision": "ecc601d6eb2980546db68e863d597e84",
+    "url": "/port-deploy/static/media/cv1.ecc601d6.png"
+  },
+  {
+    "revision": "5f6ba0b07bd42f00a35671a4cf879bb2",
+    "url": "/port-deploy/static/media/cv2.5f6ba0b0.png"
+  },
+  {
+    "revision": "0e3ea2b384becbeb6c15fe7b06c6e877",
+    "url": "/port-deploy/static/media/cv3.0e3ea2b3.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/port-deploy/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/port-deploy/static/media/git.c31b5069.svg"
+  }
+]);
