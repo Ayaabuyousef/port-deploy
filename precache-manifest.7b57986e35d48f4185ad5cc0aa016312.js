9572aa6c99578c79f91cedc9f93515c2b1a70d82
aya abuyousef <aya.abuyousef@gmail.com>
2021-01-20 17:29:13 +0200

    Updates

---
+++ precache-manifest.7b57986e35d48f4185ad5cc0aa016312.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f60be8c410a9d9cf9a091991f18ded4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ca144f4e8f37dc66fa",
+    "url": "/static/css/2.efd9d441.chunk.css"
+  },
+  {
+    "revision": "1fce5ef7e019e8f1810c",
+    "url": "/static/css/main.0015c3d2.chunk.css"
+  },
+  {
+    "revision": "13ca144f4e8f37dc66fa",
+    "url": "/static/js/2.ed86a26e.chunk.js"
+  },
+  {
+    "revision": "1fce5ef7e019e8f1810c",
+    "url": "/static/js/main.ab564820.chunk.js"
+  },
+  {
+    "revision": "6dccc698e8d644def7e1",
+    "url": "/static/js/runtime-main.0ac1f833.js"
+  },
+  {
+    "revision": "aa7581c943bf241bdb7ee95822da621f",
+    "url": "/static/media/cv.aa7581c9.png"
+  },
+  {
+    "revision": "ecc601d6eb2980546db68e863d597e84",
+    "url": "/static/media/cv1.ecc601d6.png"
+  },
+  {
+    "revision": "5f6ba0b07bd42f00a35671a4cf879bb2",
+    "url": "/static/media/cv2.5f6ba0b0.png"
+  },
+  {
+    "revision": "0e3ea2b384becbeb6c15fe7b06c6e877",
+    "url": "/static/media/cv3.0e3ea2b3.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/static/media/git.c31b5069.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/port-deploy/precache-manifest.6ede684fb8f3be27b3723957f3da5680.js"
+  "/precache-manifest.7b57986e35d48f4185ad5cc0aa016312.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/port-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
