da09c1dfaa2655f853ededc7e881d58772ce9429
aya abuyousef <aya.abuyousef@gmail.com>
2021-01-21 16:58:20 +0200

    i..

--- src/components/cv_img/index.js
+++ src/components/cv_img/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import '../cv_img/style.css';
 import cv1 from '../../assets/img/cv1.png';
-import cv2 from '../../assets/img/cv2.png';
-import cv3 from '../../assets/img/cv3.png';
+// import cv2 from '../../assets/img/cv2.png';
+// import cv3 from '../../assets/img/cv3.png';
 // import cv4 from '../../assets/img/cv4.png';
-import cv from '../../assets/img/cv.png';
+// import cv from '../../assets/img/cv.png';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css'; 
 import Roll from 'react-reveal/Roll';
@@ -12,7 +12,7 @@ import Zoom from 'react-reveal/Zoom';
 import Flip from 'react-reveal/Flip';
 
 const images = [
-  cv1,cv2,cv3
+  cv1
 ];
 
 export default class CRI extends Component {
--- src/components/cv_left_content/index.js
+++ src/components/cv_left_content/index.js
@@ -7,8 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHdd } from '@fortawesome/free-solid-svg-icons';
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import cv1 from '../../assets/img/cv1.png';
-import cv2 from '../../assets/img/cv2.png';
-import cv3 from '../../assets/img/cv3.png';
+// import cv2 from '../../assets/img/cv2.png';
+// import cv3 from '../../assets/img/cv3.png';
 // import cv4 from '../../assets/img/cv4.png';
 import Lightbox from 'react-image-lightbox';
 import {Modal, Button} from 'react-bootstrap';
@@ -16,7 +16,7 @@ import Projects from '../projects';
 import {Link, useHistory} from 'react-router-dom';
 
 const images = [
-  cv1,cv2,cv3
+  cv1
 ];
 
 const ColoredLine = ({ color }) => (
